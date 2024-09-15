eebc6936a4509a26ae9167bff225ef7ac068ac2c
Ulises <umartinezadon@outlook.com>
2024-09-15 09:57:14 -0400

    feat: git init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import data from "./data.json";
+
+/**
+ *  Rename all properties.
+ * @param {String} jsonString the json string
+ */
+function renameProperties(jsonString) {
+  return jsonString
+    .replaceAll('"type"', '"@type"')
+    .replaceAll('"referredType"', '"@referredType"')
+    .replaceAll('"characteristics"', '"characteristic"')
+    .replaceAll('"partyCharacteristics"', '"partyCharacteristic"')
+    .replaceAll('"contactMediums"', '"contactMedium"')
+    .replaceAll('"relatedParties"', '"relatedParty"')
+    .replaceAll('"individualIdentifications"', '"individualIdentification"')
+    .replaceAll(
+      '"organizationIdentifications"',
+      '"organizationIdentification"'
+    );
+}
+
+/**
+ * Transform the json string.
+ * @param {String} jsonString the json string
+ * @returns
+ */
+function transform(jsonString) {
+  if (!jsonString) {
+    return jsonString;
+  }
+  return renameProperties(jsonString);
+}
 
 function App() {
+  let [dataJson, setDataJson] = useState({ ...data });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="Input-Container">
+        <textarea
+          value={JSON.stringify(dataJson, undefined, 4)}
+          onChange={(e) => {
+            try {
+              setDataJson(JSON.parse(e.target.value ?? "{}"));
+            } catch (e) {
+              setDataJson(JSON.stringify(e.target));
+            }
+          }}
+        ></textarea>
+        <button
+          class="mdc-button"
+          onClick={(e) => {
+            navigator.clipboard
+              .readText()
+              .then((text) => {
+                setDataJson(JSON.parse(text));
+              })
+              .catch((err) => {
+                console.error("Failed to read clipboard contents: ", err);
+              });
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <div class="mdc-button__ripple"></div>
+          <span class="mdc-button__label">PASTE</span>
+        </button>
+      </div>
+      <div className="Input-Container">
+        <textarea
+          className="output"
+          value={transform(
+            JSON.stringify(
+              dataJson && dataJson.arg0 ? dataJson.arg0 : dataJson,
+              undefined,
+              4
+            )
+          )}
+          disabled
+        ></textarea>
+        <button
+          class="mdc-button"
+          onClick={() => {
+            document.querySelector(".output").select();
+            document.execCommand("copy");
+          }}
+        >
+          <div class="mdc-button__ripple"></div>
+          <span class="mdc-button__label">COPY</span>
+        </button>
+      </div>
     </div>
   );
 }
